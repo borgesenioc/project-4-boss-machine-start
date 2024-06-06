4ef55fee407265370b558b12efe4fcdb088d75a6
borgesenioc <borgesenioc@gmail.com>
2024-06-06 09:21:15 -0300

    Add GET method to fetch all ideas

--- server/api.js
+++ server/api.js
@@ -67,4 +67,14 @@ apiRouter.delete('/minions/:minionId', (req, res, next) => {
     }
 });
 
+//GET /api/ideas to get an array of all ideas.
+apiRouter.get('/ideas', (req, res, next) => {
+    const allIdeas = getAllFromDatabase('ideas');
+    res.send(allIdeas)
+});
+//POST /api/ideas to create a new idea and save it to the database.
+//GET /api/ideas/:ideaId to get a single idea by id.
+//PUT /api/ideas/:ideaId to update a single idea by id.
+//DELETE /api/ideas/:ideaId to delete a single idea by id.
+
 module.exports = apiRouter;
