3d22160771d37e90d9c5d1ae438e02e557c5d3c3
borgesenioc <borgesenioc@gmail.com>
2024-06-08 09:58:08 -0300

    Add checkMillionDollarIdea middleware function

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,18 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+    const { numWeeks, weeklyRevenue } = req.body;
+
+    // Check if numWeeks and weeklyRevenue are valid numbers
+    if (!numWeeks || !weeklyRevenue || isNaN(numWeeks) || isNaN(weeklyRevenue)) {
+        return res.status(400).send();
+    }
+
+    const totalValue = numWeeks * weeklyRevenue;
+    if (totalValue >= 1000000) {
+        next();
+    } else {
+        res.status(400).send();
+    }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
